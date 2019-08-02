093d9bc4005a5de8bb0afae77e198bc437def912
Diogo <drd.rclrd@gmail.com>
2019-08-01 21:37:22 -0300

    create a pure function for both components

---
+++ src/components/CommentBox.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CommentBox() {
+  return (
+    <div>
+      CommentBox
+    </div>
+  )
+}
---
+++ src/components/CommentList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CommentList() {
+  return (
+    <div>
+      CommentList
+    </div>
+  )
+}
