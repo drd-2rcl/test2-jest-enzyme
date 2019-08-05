ca7f488490dacad2b49ae2140ced93e1866e5128
Diogo <drd.rclrd@gmail.com>
2019-08-05 17:59:51 -0300

    simulate a change event, update the component and assert value

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -6,11 +6,28 @@ let wrapped;
 
 beforeEach(() => {
   wrapped = mount(<CommentBox />)
-})
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
 
 it('has a text area and a button', () => {
-  // console.log(wrapped.find('textarea').length);
-  // console.log(wrapped.find('button'));
+  // find the textarea element
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
+});
+
+it('has a text area that users can type in', () => {
+  // simulate a change event
+  wrapped.find('textarea').simulate('change', {
+    // provide a fake event object
+    target: { value: 'new comment' }
+  });
+
+  // force the component to update
+  wrapped.update()
+
+  // assert that the textareas value has changed
+  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
 });
