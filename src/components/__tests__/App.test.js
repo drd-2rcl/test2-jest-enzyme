67ab9c2b796d9372ed9a3164c0e91ebdc330e9ff
Diogo <drd.rclrd@gmail.com>
2019-08-05 11:51:26 -0300

    added test for component CommentList

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import App from '../App';
+import App from 'components/App';
 import { shallow } from 'enzyme';
-import CommentBox from '../CommentBox'
+import CommentBox from 'components/CommentBox'
+import CommentList from 'components/CommentList'
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
 
 it('shows a comment box', () => {
-  const wrapped = shallow(<App />);
-
   expect(wrapped.find(CommentBox)).toHaveLength(1);
+});
 
+it('shows a comment list', () => {
+  expect(wrapped.find(CommentList)).toHaveLength(1);
 });
