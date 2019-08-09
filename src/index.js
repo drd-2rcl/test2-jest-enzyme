284a4353f1a1b89063a02e34abe596f529d9949e
Diogo <drd.rclrd@gmail.com>
2019-08-09 14:15:40 -0300

    refactoring for reuse provider in component and test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from 'reducers';
+import Root from './Root.js';
 
 ReactDOM.render(
-  <Provider store={createStore(reducers, {})}>
+  <Root>
     <App />
-  </Provider>, document.getElementById('root'));
+  </Root>, 
+  document.getElementById('root')
+);
