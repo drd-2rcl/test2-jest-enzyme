a8277eb6ad174922fd942790f9a9a71fc3bd4b2e
Diogo <drd.rclrd@gmail.com>
2019-08-13 17:33:42 -0300

    refactoring to receive comments inside render

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
 
-export default function CommentList() {
-  return (
-    <div>
-      CommentList
-    </div>
-  )
+class CommentList extends Component {
+  renderComments() {
+    return this.props.comments.map(comment => {
+      return <li key={comment}>{comment}</li>;
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.renderComments()}
+        </ul>
+      </div>
+    )
+  }
 }
+
+function mapStateToProps(state) {
+  return { comments: state.comments };
+}
+
+export default connect(mapStateToProps)(CommentList);
