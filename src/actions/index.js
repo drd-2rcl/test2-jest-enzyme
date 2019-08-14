729f67f93f1a143e59ea89547c12ea405b5d88fb
Diogo <drd.rclrd@gmail.com>
2019-08-14 10:56:26 -0300

    created action for fetch comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,17 @@
-import { SAVE_COMMENT } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
 
 export function saveComment(comment) {
   return {
     type: SAVE_COMMENT,
     payload: comment
+  }
+}
+
+export function fetchComments() {
+  const response = axios.get('https://jsonplaceholder.typicode.com/comments');
+
+  return {
+    type: FETCH_COMMENTS,
+    payload: response
   }
 }
