f2705c1c9b576a7285eae0493ad0e516a0f13f54
Diogo <drd.rclrd@gmail.com>
2019-08-13 23:09:59 -0300

    created test for new <li> and show text

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -8,7 +8,7 @@ let wrapped;
 
 beforeEach(() => {
   const initialState = {
-    comments: ['Comment 1', 'Comment2']
+    comments: ['Comment 1', 'Comment 2']
   }
 
   wrapped = mount(
@@ -19,5 +19,10 @@ beforeEach(() => {
 })
 
 it('creators one LI per comment', () => {
-  console.log(wrapped.find('li').length);
+  expect(wrapped.find('li').length).toEqual(2);
+})
+
+it('shows the text for each comment', () => {
+  expect(wrapped.render().text()).toContain('Comment 1');
+  expect(wrapped.render().text()).toContain('Comment 2');
 })
