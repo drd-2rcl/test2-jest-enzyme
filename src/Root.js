6a2608b07b6de6b848620e42811ca4d3661d7e19
Diogo <drd.rclrd@gmail.com>
2019-08-14 10:51:15 -0300

    added applymiddleware in store

--- src/Root.js
+++ src/Root.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from 'reducers';
+import reduxPromise from 'redux-promise';
 
 export default ({ children, initialState = {} }) => {
+  const store = createStore(reducers, initialState, applyMiddleware(reduxPromise));
   return (
-    <Provider store={createStore(reducers, initialState)}>
+    <Provider store={store}>
       {children}
     </Provider>
   )
-}
+}  
