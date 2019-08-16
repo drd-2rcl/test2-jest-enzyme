8d1e2ee49e91a07d995f762a714eeaca58f7e2a0
Diogo <drd.rclrd@gmail.com>
2019-08-16 19:46:44 -0300

    was added render method for header and button

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import { Route } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom'
 import CommentBox from './CommentBox';
 import CommentList from './CommentList';
 
-function App() {
-  return (
-    <div>
-      <Route path="/" exact component={CommentList} />
-      <Route path="/post" component={CommentBox} />
-    </div>
-  );
+export default class App extends Component {
+  renderButton = () => {}
+
+  renderHeader = () => {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post a Comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <Route path="/" exact component={CommentList} />
+        <Route path="/post" component={CommentBox} />
+      </div>
+    );
+  }
 }
 
 export default App;
