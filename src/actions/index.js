230b0e3603c02ac49af0624b5c66af063005526e
Diogo <drd.rclrd@gmail.com>
2019-08-15 21:26:09 -0300

    created action for change auth

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from 'actions/types';
 import axios from 'axios';
 
 export function saveComment(comment) {
@@ -14,5 +14,12 @@ export function fetchComments() {
   return {
     type: FETCH_COMMENTS,
     payload: response
+  }
+}
+
+export function changeAuth(isLoggedIn) {
+  return {
+    type: CHANGE_AUTH,
+    payload: isLoggedIn
   }
 }
